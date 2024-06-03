0615ee2a5424d845dbb807c62f26b25ac4ae9e2c
Juntao Deng <74239179+ddd9898@users.noreply.github.com>
2024-06-03 12:50:28 +0800

    Add files via upload

---
+++ utils/dataloader.py
@@ -0,0 +1,262 @@
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+from Bio import SeqIO
+import random
+from scipy.spatial.transform import Rotation as R
+import torch
+from torch import nn
+
+class seqData_Dscript(Dataset):
+    def __init__(self, pair_path, seqs_path, addNeg=True,augment=False):
+        super(seqData_Dscript,self).__init__()
+        
+        self.seq_data = list()
+        self.augment = augment
+        
+        ##Load sequence data
+        PPI_seq_dict = dict()
+        for fa in SeqIO.parse(seqs_path,'fasta'):
+            ID = fa.description
+            seq = ''.join(list(fa.seq))
+            PPI_seq_dict[ID] = seq
+        
+        ##Load pair data
+        data = pd.read_csv(pair_path,sep='\t',header=None).values.tolist()
+        
+        for item in data:
+            ID1,ID2,label = item
+            
+            if (not addNeg) and label == 0:
+                continue
+            
+            seq1 = PPI_seq_dict[ID1]
+            seq2 = PPI_seq_dict[ID2]
+            
+            if len(seq1)>800 or len(seq2)>800:
+                    continue
+            
+            self.seq_data.append([seq1,seq2,label])
+
+
+    def __len__(self):
+        return len(self.seq_data)
+
+    def __getitem__(self,i):
+        seq1,seq2,label = self.seq_data[i]
+       
+        if not self.augment:
+            return seq1,seq2,label
+        else:
+            p = random.random()
+            if p<0.5:
+                return seq1,seq2,label
+            else:
+                return seq2,seq1,label
+
+    
+    
+
+class seqData_NBAT(Dataset):
+    def __init__(self, pair_path, split_path, data_split='0-train', addNeg=True):
+        super(seqData_NBAT,self).__init__()
+        
+        self.seq_data = list()
+
+        idx, split = data_split.split('-')
+        
+        ##Load pair data
+        data = pd.read_csv(pair_path).values.tolist()
+        split_data = pd.read_csv(split_path).values.tolist()[int(idx)]
+        
+        for n,item in enumerate(data):
+            label,seq1,seq2 = item
+            if label == 'Yes':
+                label = 1
+            else:
+                label = 0
+            
+            if (not addNeg) and label == 0:
+                continue
+
+            if (split=='train' and split_data[n]==0) or (split=='test' and split_data[n]==1):
+                self.seq_data.append([seq1,seq2,label])
+
+
+    def __len__(self):
+        return len(self.seq_data)
+
+    def __getitem__(self,i):
+        seq1,seq2,label = self.seq_data[i]
+       
+        return seq1,seq2,label
+
+class seqData_NBAT_Test(Dataset):
+    def __init__(self,seq_path,pair_path):
+        super(seqData_NBAT_Test,self).__init__()
+        
+        self.seq_data = list()
+        
+        ##Load all PPI sequence
+        PPI_seq_dict = dict()
+        # for fa in SeqIO.parse('./data/Nanobody_Antigen-main/all_pair_data.seqs.fasta','fasta'):
+        for fa in SeqIO.parse(seq_path,'fasta'):
+            ID = fa.description
+            seq = ''.join(list(fa.seq))
+            PPI_seq_dict[ID] = seq
+        
+        ##Load pair data
+        # data = pd.read_csv('./data/Nanobody_Antigen-main/all_pair_data.pair.tsv',sep='\t').values.tolist()
+        data = pd.read_csv(pair_path,sep='\t',header=None).values.tolist()
+        
+        for n,item in enumerate(data):
+            ID1, ID2, label = item
+            seq1 = PPI_seq_dict[ID1]
+            seq2 = PPI_seq_dict[ID2]
+            
+            if len(seq1)>800 or len(seq2)>800:
+                continue
+            
+            self.seq_data.append([seq1,seq2,label])
+
+    def __len__(self):
+        return len(self.seq_data)
+    def __getitem__(self,i):
+        seq1,seq2,label = self.seq_data[i]
+       
+        return seq1,seq2,label
+
+    
+def split_Train_Test(data_path,test_ratio=0.1):
+    '''
+    data_path: 数据路径,data中每行为一个样本
+    Return: TrainData,TestData
+    '''
+    ##Load pair data
+    data = pd.read_csv(data_path).values.tolist()
+
+    ##Split Train and Test
+    idx_list = list(range(len(data)))
+    random.shuffle(idx_list)
+    Test_Num = int(len(data)*test_ratio)
+    train_list = idx_list[Test_Num:]
+    test_list = idx_list[:Test_Num]
+    
+    # #Debug
+    # train_list = idx_list[100:200]
+    # test_list = idx_list[:100]
+    
+    trainData = [data[idx] for idx in train_list]
+    testData =  [data[idx] for idx in test_list]
+        
+    return trainData,testData
+    
+
+class seqData_Sabdab(Dataset):
+    def __init__(self, data, addNeg=True):
+        super(seqData_Sabdab,self).__init__()
+        
+        # data = pd.read_csv(pair_path).values.tolist()
+        self.seq_data = list()
+        for item in data:
+            _,_,seq1,_,_,seq2,_,_,_ = item
+            if len(seq1)>800 or len(seq2)>800:
+                continue
+            self.seq_data.append([seq1,seq2,1])
+
+        ##Add negative
+        seq_data_neg = list()
+        candidates = list(range(len(self.seq_data)))
+        if addNeg:
+            for idx1 in candidates:
+                for t in range(10):
+                    idx2 = random.choice(candidates)
+                    seq_data_neg.append([self.seq_data[idx2][0],self.seq_data[idx1][1],0])
+                
+            self.seq_data.extend(seq_data_neg)
+
+
+    def __len__(self):
+        return len(self.seq_data)
+
+    def __getitem__(self,i):
+        seq1,seq2,label = self.seq_data[i]
+
+        return seq1,seq2,label
+
+class infaData_Sabdab(Dataset):
+    def __init__(self, data, augment=False, addNeg=True):
+        super(infaData_Sabdab,self).__init__()
+        
+        # data = pd.read_csv(pair_path).values.tolist()
+        self.seq_data = list()
+        for item in data:
+            _,_,seq1,_,_,seq2,BSite2,_,_ = item
+            if len(seq1)>800 or len(seq2)>800:
+                continue
+            BSite2 = self.__augmentBSite(BSite2,seq2,augment=augment)
+            self.seq_data.append([seq1,seq2,1,BSite2])
+        
+        ##Add negative
+        seq_data_neg = list()
+        candidates = list(range(len(self.seq_data)))
+        if addNeg:
+            for idx1 in candidates:
+                for t in range(10):
+                    idx2 = random.choice(candidates)
+                    seq_data_neg.append([self.seq_data[idx2][0],self.seq_data[idx1][1],0,self.seq_data[idx1][3]])
+                
+            self.seq_data.extend(seq_data_neg)
+
+    def __len__(self):
+        return len(self.seq_data)
+
+    def __getitem__(self,i):
+        seq1,seq2,label,BSite2 = self.seq_data[i]
+       
+        return seq1,seq2,label,BSite2
+
+    def __augmentBSite(self,BSite2,seq2,augment=True):
+        BSite2 = [int(idx) for idx in BSite2.split(',')]
+        ##对结合界面进行数据增广
+        max_len = len(seq2)
+        if augment:
+            num_add = random.choice([0,1,2,3,4,5])
+        else:
+            num_add = 0
+        add_idxs = list()
+        for idx in BSite2:
+            for new_idx in range(idx-num_add,idx+num_add+1):
+                if new_idx>=0 and new_idx<max_len and new_idx not in BSite2:
+                    add_idxs.append(new_idx)
+        BSite2.extend(add_idxs)
+
+        temp = np.zeros(len(seq2))
+        for idx in BSite2:
+            temp[idx] = 1
+        BSite2 = list(temp)
+
+        return BSite2
+
+
+
+def collate_fn_infaData(batch):
+    
+    seq1_batch = [item[0] for item in batch]
+    
+    seq2_batch = [item[1] for item in batch]
+    
+    label = torch.FloatTensor([item[2] for item in batch])
+
+    
+    
+    max_len = max([len(item[3]) for item in batch])
+    aa_BSite2_batch = list()
+    for item in batch:
+        BSite2 = torch.LongTensor(np.pad(item[3],(0,max_len-len(item[3])),'constant')).unsqueeze(0)
+        aa_BSite2_batch.append(BSite2)
+
+    aa_BSite2_batch = torch.cat(aa_BSite2_batch,dim=0)
+    
+
+    return seq1_batch,seq2_batch,label,aa_BSite2_batch
---
+++ utils/evaluate.py
@@ -0,0 +1,53 @@
+from sklearn.metrics import roc_auc_score,accuracy_score,f1_score,precision_score,recall_score,precision_recall_curve,auc
+import numpy as np
+
+def evaluate(GT,Pre,thresh = 0.5):
+    GT = np.array(GT)
+    Pre = np.array(Pre)
+    
+    #Top 10/20/50
+    idx_list = np.argsort(-Pre)
+    GT_new = [GT[idx] for idx in idx_list]
+    
+
+    Top10 = np.sum(GT_new[:10])/10
+    Top20 = np.sum(GT_new[:20])/20
+    Top50 = np.sum(GT_new[:50])/50
+    
+    AUC_ROC = roc_auc_score(GT,Pre)
+    precision_list, recall_list, _ = precision_recall_curve(GT, Pre)
+    AUC_PR = auc(recall_list, precision_list)
+
+    # Pre = [1 if item>0.5 else 0 for item in Pre]
+    Pre = [1 if item>thresh else 0 for item in Pre]
+    accuracy = accuracy_score(GT,Pre)
+    recall = recall_score(GT,Pre)
+    precision = precision_score(GT,Pre)
+    F1_score = f1_score(GT,Pre,average='macro')
+    
+    return precision,recall,accuracy,F1_score,Top10,Top20,Top50,AUC_ROC,AUC_PR
+ 
+
+def evaluate_site(labels,BSites,thresh = 0.5):
+    
+    accuracy = 0
+    recall = 0
+    precision = 0
+    F1_score = 0
+    for n in range(len(labels)):
+        GT = np.array(labels[n])
+        Pre = np.array(BSites[n])
+        Pre = np.array([1 if item>thresh else 0 for item in Pre])
+        
+        accuracy += accuracy_score(GT,Pre)
+        recall += recall_score(GT,Pre)
+        precision += precision_score(GT,Pre)
+        F1_score += f1_score(GT,Pre,average='macro')
+        
+    accuracy = accuracy/len(labels)
+    recall = recall/len(labels)
+    precision = precision/len(labels)
+    F1_score = F1_score/len(labels)
+
+    return accuracy,precision,recall,F1_score
+ 
